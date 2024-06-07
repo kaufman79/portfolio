177f452024e1df7f8afcf641d90f79a5d28f681b
kaufman79 <kaufman79@gmail.com>
2024-06-07 17:12:09 -0400

    input photo, about me, and contact me text

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/profile pic.jpg", width = 450)
+
+
+with col2:
+    st.title("Ryan A. Kaufman")
+    content = """
+    Hello, I am Ryan! I am a developer and Latin teacher -- quite the combination, I am aware! I suppose it was bound to
+    be this way, operating in polar extremes; I also love classical music...as well as heavy metal; I love poetry and 
+    literature, but at the same time, am a fan of mixed martial arts. You get the idea. I have a B.A. in Biblical 
+    Studies from Geneva College, and a Master of Divinity from Reformed Theological Seminary.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I've built. Feel free to contact me by clicking on 'contact me' on the left side of this page!
+"""
+st.write(content2)
