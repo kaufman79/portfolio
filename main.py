114bbd0f90ffa64947c925436194489bb90b5faa
kaufman79 <kaufman79@gmail.com>
2024-06-08 15:33:46 -0400

    add app description, image, and links

--- main.py
+++ main.py
@@ -24,14 +24,20 @@ Below you can find some of the apps I've built. Feel free to contact me by click
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, spacer_col, col4 = st.columns([1.5, 0.5, 1.5]) # list of ratio dimensions for the argument
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image ("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
